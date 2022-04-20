8643dbe6799fd7e6af96bf699a04733ad10132a5
Victoriya <79446102+rxndx@users.noreply.github.com>
2022-04-20 12:43:19 +0300

    Add files via upload

---
+++ src/test/java/example/enkrazh/EnkrazhApplicationTests.java
@@ -0,0 +1,13 @@
+package example.enkrazh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnkrazhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
