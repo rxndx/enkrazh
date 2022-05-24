0b3bdfeb3f7326bef5dde539e652ac76270e0540
Mariia Sirenko <79446249+mariiasirenko2@users.noreply.github.com>
2022-05-24 12:36:47 +0300

    Delete src directory

--- src/main/java/com/enkrazh/enkrazh/repo/PlayerProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.enkrazh.enkrazh.repo;
-
-import com.enkrazh.enkrazh.model.PlayerProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerProfileRepository extends JpaRepository<PlayerProfile,Integer> {
-}
