af0f026244279c18128ec3382ee92f035bd48d43
Victoriya <kisa6485@gmail.com>
2022-06-03 16:45:58 +0300

    Ready second page + fix modal

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -1,21 +1,26 @@
-<!-- Только одно модальное окно.. -->
+<!-- Это надо переписать -->
 
-let modal = document.querySelector(".modal");
-let closeButton = document.querySelector(".close-button");
-let show = document.querySelector('.show')
+let modal1 = document.getElementById("modal-1");
+let modal2 = document.getElementById("modal-2");
+let closeButton1 = document.getElementById("close-1");
+let closeButton2 = document.getElementById("close-2");
+let show1 = document.getElementById('button-1')
+let show2 = document.getElementById('button-2')
 
-function toggleModal() {
-    modal.classList.toggle("show-modal");
+function toggleModal(event) {
+    if (event.target === show1) { modal1.classList.toggle("show-modal"); }
+    if (event.target === show2) { modal2.classList.toggle("show-modal"); }
 }
 
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
+function closeModal(event) {
+    if (event.target !== modal1) { modal1.classList.remove("show-modal");
+        document.body.style.position = '';}
+    if (event.target !== modal2) { modal2.classList.remove("show-modal"); }
 }
 
-show.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
+show1.addEventListener("click", toggleModal);
+show2.addEventListener("click", toggleModal);
+closeButton1.addEventListener("click", closeModal);
+closeButton2.addEventListener("click", closeModal);
 
 // TODO: функция блока прокрутки при возникновении модального окна
