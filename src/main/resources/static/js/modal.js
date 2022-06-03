530a11a2f5f776e49e6bcb9a8e6804b18b34296b
Victoriya <kisa6485@gmail.com>
2022-06-03 19:13:03 +0300

    fixed lock modal

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -8,19 +8,20 @@ let show1 = document.getElementById('button-1')
 let show2 = document.getElementById('button-2')
 
 function toggleModal(event) {
-    if (event.target === show1) { modal1.classList.toggle("show-modal"); }
-    if (event.target === show2) { modal2.classList.toggle("show-modal"); }
+    if (event.target === show1) { modal1.classList.toggle("show-modal");
+        document.body.style.display = 'fixed'; document.body.style.overflow = 'hidden'; }
+    if (event.target === show2) { modal2.classList.toggle("show-modal");
+        document.body.style.display = 'fixed'; document.body.style.overflow = 'hidden';}
 }
 
 function closeModal(event) {
     if (event.target !== modal1) { modal1.classList.remove("show-modal");
-        document.body.style.position = '';}
-    if (event.target !== modal2) { modal2.classList.remove("show-modal"); }
+        document.body.style.position = ''; document.body.style.display = ''; document.body.style.overflow = ''; }
+    if (event.target !== modal2) { modal2.classList.remove("show-modal");
+        document.body.style.position = ''; document.body.style.display = ''; document.body.style.overflow = '';}
 }
 
 show1.addEventListener("click", toggleModal);
 show2.addEventListener("click", toggleModal);
 closeButton1.addEventListener("click", closeModal);
 closeButton2.addEventListener("click", closeModal);
-
-// TODO: функция блока прокрутки при возникновении модального окна
